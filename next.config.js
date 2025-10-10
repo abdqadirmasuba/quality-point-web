bcdc54ea1a65d790a0c80c05a235e6a36a8a288d
abdqadir <masubaabdq@gmail.com>
2025-10-10 15:04:14 +0300

    config changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',       // enables static export
-  basePath: 'quality-point-web',
-  assetPrefix: 'quality-point-web/',
+  basePath: '/quality-point-web',
+  assetPrefix: '/quality-point-web/',
   // distDir: 'dist',        // everything (build + export) goes here
   images: { unoptimized: true }  // required for static export
 };
