cc3f00bcf19e54692a584c32f0c391696b515f15
abdqadir <masubaabdq@gmail.com>
2025-10-10 15:11:56 +0300

    config changes dist file

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export',       // enables static export
   basePath: '/quality-point-web',
   assetPrefix: '/quality-point-web/',
-  // distDir: 'dist',        // everything (build + export) goes here
+  distDir: 'dist',        // everything (build + export) goes here
   images: { unoptimized: true }  // required for static export
 };
 
